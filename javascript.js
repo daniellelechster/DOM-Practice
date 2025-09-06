ce0130cf361038277f055dc5ee7f8a4d4a78e154
Danielle Siegel <daniellesiegel78@gmail.com>
2025-09-06 16:25:20 -0400

    Set attribute seasonal image changes based on theme

--- javascript.js
+++ javascript.js
@@ -1,3 +1,18 @@
+let isPromoApplied = false;
+
+
+
+
+
+
+
 const changeTheme = () => {
+    isPromoApplied = !isPromoApplied
     document.body.classList.toggle("seasonal-promo")
+
+    if(isPromoApplied) {
+        document.querySelector("#seasonal-promo-img").setAttribute("src", "./assets/images/matcha.jpg")
+    }
 }
+
+// &&(AND) ||(OR) !(NOT)//
