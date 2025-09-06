4e962de81c04e0a96470393393a8a3e510b41fb1
Danielle Siegel <daniellesiegel78@gmail.com>
2025-09-06 16:43:16 -0400

    moved

--- javascript.js
+++ javascript.js
@@ -1,18 +1,28 @@
+const promoApplication = () => {
+
+let seasonalPromo = document.querySelector("#seasonal-promo")
 let isPromoApplied = false;
 
+    const changeTheme = () => {
+        isPromoApplied = !isPromoApplied
+        document.body.classList.toggle("seasonal-promo")
 
-
-
-
-
-
-const changeTheme = () => {
-    isPromoApplied = !isPromoApplied
-    document.body.classList.toggle("seasonal-promo")
-
-    if(isPromoApplied) {
-        document.querySelector("#seasonal-promo-img").setAttribute("src", "./assets/images/matcha.jpg")
+        if(isPromoApplied) {
+            document.querySelector("#seasonal-promo-img").setAttribute("src", "./assets/images/matcha.jpg")
+        } else {
+            document.querySelector("#seasonal-promo-img").setAttribute("src", "./assets/images/psl.jpg")
+        }
     }
-}
 
+    seasonalPromo.addEventListener("click", ()=> {
+        changeTheme()
+    })
+
+
+
+
+
+}
 // &&(AND) ||(OR) !(NOT)//
+
+promoApplication()
